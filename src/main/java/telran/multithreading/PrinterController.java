46c1c5d04d8e40bdd757feb8bafe03ed0d06a9a0
Mark <kazhdan.mark@gmail.com>
2024-11-15 14:20:01 +0200

    CW41

---
+++ src/main/java/telran/multithreading/PrinterController.java
@@ -0,0 +1,11 @@
+package telran.multithreading;
+
+public class PrinterController {
+    public static void main(String[] args) {
+        Printer printer1 = new Printer('#');
+        Printer printer2 = new Printer('*');
+        printer1.start();
+        printer2.start();
+    }
+}
+
